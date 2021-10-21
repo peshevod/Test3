a7307170ce9a90db3c57cd7ed8c27d1f7663b40c
Ilya Shugalev <ilya@shugalev.com>
2021-10-21 21:48:58 +0300

    MyAsyncConnection probe

--- app/src/main/java/com/example/test3/MyAsyncConnection.java
+++ app/src/main/java/com/example/test3/MyAsyncConnection.java
@@ -4,9 +4,17 @@ import android.os.StrictMode;
 import android.util.Log;
 
 import org.apache.hc.client5.http.HttpRoute;
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequests;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.async.methods.SimpleRequestProducer;
+import org.apache.hc.client5.http.async.methods.SimpleResponseConsumer;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
 import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
 import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.nio.AsyncConnectionEndpoint;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
 import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
 import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
 import org.apache.hc.core5.concurrent.FutureCallback;
@@ -17,6 +25,7 @@ import org.apache.hc.core5.http.HttpRequest;
 import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.impl.bootstrap.AsyncRequesterBootstrap;
 import org.apache.hc.core5.http.impl.bootstrap.HttpAsyncRequester;
+import org.apache.hc.core5.http.message.StatusLine;
 import org.apache.hc.core5.http.nio.ssl.TlsStrategy;
 import org.apache.hc.core5.http.protocol.BasicHttpContext;
 import org.apache.hc.core5.http.protocol.HttpContext;
@@ -33,10 +42,12 @@ import java.io.IOException;
 import java.net.SocketAddress;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLSession;
 
 public class MyAsyncConnection {
 
@@ -47,13 +58,14 @@ public class MyAsyncConnection {
     String TAG="TLS13";
     AsyncConnectionEndpoint asyncConn;
     TlsStrategy tlsStrategy;
+    CloseableHttpAsyncClient client;
 
     public MyAsyncConnection(MainActivity main) {
         this.main=main;
-        if (android.os.Build.VERSION.SDK_INT > 9) {
+/*        if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
-        }
+        }*/
         try {
             ctx = SSLContext.getInstance("TLSv1.3");
             ctx.init(null, null, null);
@@ -76,92 +88,27 @@ public class MyAsyncConnection {
         this.asyncConnMgr = PoolingAsyncClientConnectionManagerBuilder.create()
                 .setTlsStrategy(tlsStrategy)
                 .build();
+        client= HttpAsyncClients.custom()
+                .setConnectionManager(asyncConnMgr)
+                .build();
+//        client.start();
         basicHttpContext=new BasicHttpContext();
         main.login_state=MainActivity.CONNECT_REQUIRED;
     }
 
     public void asyncConnect(String hostname, int port) {
-        main.host = new HttpHost("https", hostname, port);
-        HttpRoute route = new HttpRoute(main.host);
-
-        FutureCallback<AsyncConnectionEndpoint> fcb_connect = new FutureCallback<AsyncConnectionEndpoint>() {
-
-            @Override
-            public void completed(AsyncConnectionEndpoint result) {
-                main.connected = true;
-                main.homeViewModel.postConnected(main.connected);
-                Log.i("TLS13", "Connected!");
-                asyncConnMgr.release(result, null, TimeValue.ofHours(1));
-                main.login_state = MainActivity.BASIC_LOGIN_REQUIRED;
-            }
-
-            @Override
-            public void failed(Exception ex) {
-                main.connected = false;
-                main.homeViewModel.postConnected(main.connected);
-                Log.e(TAG+" fcb_connect", ex.getMessage());
-            }
-
-            @Override
-            public void cancelled() {
-                main.connected = false;
-                main.homeViewModel.postConnected(main.connected);
-                Log.e(TAG+" fcb_connect", "Connection cancelled");
-            }
-        };
+//        main.host = new HttpHost("https", hostname, port);
+//        final HttpClientContext clientContext = HttpClientContext.create();
 
 
-        FutureCallback<AsyncConnectionEndpoint> fcb_lease = new FutureCallback<AsyncConnectionEndpoint>() {
-            @Override
-            public void completed(AsyncConnectionEndpoint result) {
-                asyncConn = result;
-                if (!asyncConn.isConnected()) {
-                    Log.i(TAG, "No connection yet, connecting...");
-/*                    asyncConnMgr.connect(asyncConn, new ConnectionInitiator(){
-                        public Future<IOSession> connect(NamedEndpoint remoteEndpoint, SocketAddress remoteAddress, SocketAddress localAddress, Timeout timeout, Object attachment, FutureCallback<IOSession> callback) {
-                            return null;
-                        }
-                    }, Timeout.ofMilliseconds(5000), null, new BasicHttpContext(), fcb_connect);*/
-                    HttpAsyncRequester ci= AsyncRequesterBootstrap
-                            .bootstrap()
-/*                            .setTlsStrategy(tlsStrategy)
-                           .setConnectionReuseStrategy(new ConnectionReuseStrategy() {
-                                @Override
-                                public boolean keepAlive(HttpRequest request, HttpResponse response, HttpContext context) {
-                                    return true;
-                                }
-                            })
-                            .setIOReactorConfig(IOReactorConfig.custom().setIoThreadCount(3).build())*/
-                            .create();
-//                    DefaultConnectingIOReactor ci=new DefaultConnectingIOReactor(null);
-                    ci.start();
-                    asyncConnMgr.connect(asyncConn, ci , Timeout.ofMilliseconds(5000),
-                            null, new BasicHttpContext(), fcb_connect);
-                }
-                else
-                {
-                    main.connected = true;
-                    main.homeViewModel.postConnected(main.connected);
-                    Log.i(TAG, "Connection is already opened");
-                }
-            }
 
-            @Override
-            public void failed(Exception ex) {
-                Log.e(TAG, ex.getMessage());
-                main.connected = false;
-                main.homeViewModel.postConnected(main.connected);
-                return;
-            }
-
-            @Override
-            public void cancelled() {
-                main.connected = false;
-                main.homeViewModel.postConnected(main.connected);
-                Log.e(TAG+" fcb_lease", "Connection cancelled");
-            }
-        };
-        asyncConnMgr.lease("1", route, null, Timeout.ofMilliseconds(3000), fcb_lease);
+        MyCloseableHttpAsyncClient myClient=new MyCloseableHttpAsyncClient(main, hostname, port, client);
+        myClient.run();
+        try {
+            myClient.future.get();
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG+ " asyncConnect",e.getMessage());
+        }
     }
 
     public void asyncDisconnect()
